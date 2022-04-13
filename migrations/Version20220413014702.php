c1f185fc0b048ef8df6657333f2a31409774b456
massndongo86@gmail.com <massndongo86@gmail.com>
2022-04-13 02:00:18 +0000

    first commit

---
+++ migrations/Version20220413014702.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220413014702 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE facture (id INT AUTO_INCREMENT NOT NULL, date_facture DATE NOT NULL, number_invoce INT NOT NULL, customer_id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE invocelines (id INT AUTO_INCREMENT NOT NULL, invoce_id INT DEFAULT NULL, description VARCHAR(255) DEFAULT NULL, quantity INT NOT NULL, amount NUMERIC(10, 2) DEFAULT NULL, vat_amount NUMERIC(10, 2) DEFAULT NULL, total NUMERIC(10, 2) NOT NULL, INDEX IDX_6E0696C0EED8DFC8 (invoce_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE invocelines ADD CONSTRAINT FK_6E0696C0EED8DFC8 FOREIGN KEY (invoce_id) REFERENCES facture (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE invocelines DROP FOREIGN KEY FK_6E0696C0EED8DFC8');
+        $this->addSql('DROP TABLE facture');
+        $this->addSql('DROP TABLE invocelines');
+    }
+}
